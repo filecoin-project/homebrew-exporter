f3e85a213f3a7d8de93b51b6dc4ccafc5d44a93c
Ian Davis <jungziege@gmail.com>
2022-09-02 15:42:59 -0700

    First commit -- sample metrics exporter

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+    listenAddress = flag.String("web.listen-address", ":9888", "Address to listen on for web interface.")
+    metricPath    = flag.String("web.metrics-path", "/metrics", "Path under which to expose metrics.")
+)
+
+func main() {
+    log.Fatal(serverMetrics(*listenAddress, *metricPath))
+}
+
+func serverMetrics(listenAddress, metricsPath string) error {
+    http.Handle(metricsPath, promhttp.Handler())
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte(`
+            <html>
+            <head><title>Volume Exporter Metrics</title></head>
+            <body>
+            <h1>ConfigMap Reload</h1>
+            <p><a href='` + metricsPath + `'>Metrics</a></p>
+            </body>
+            </html>
+        `))
+    })
+    return http.ListenAndServe(listenAddress, nil)
+}
